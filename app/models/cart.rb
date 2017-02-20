b565f6b1b05fcad14dfcaa0afa32c005111f9eaa
Frank Rolim <frankmendes@live.com>
2017-02-20 22:52:49 +0000

    cartrbremovedTEST

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,6 @@
-class Cart < ApplicationRecord
-  include Priceable
+#class Cart < ApplicationRecord
+#  include Priceable
 
-  belongs_to :user
-  has_many :order_items
-end
+#  belongs_to :user
+#  has_many :order_items
+#end
